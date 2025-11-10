8c46de04660d20ec20da7bc9f76707cd2c01b862
Stanislav Kishechko <skishechko@gmail.com>
2025-11-10 22:50:59 +0200

    delete intermediate variables for result

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -14,8 +14,7 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
+    assert split_integer(17, 4) == sorted(split_integer(17, 4))
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
@@ -23,8 +22,8 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
 
 
 def test_difference_between_min_and_max_should_not_exceed_one() -> None:
-    result = split_integer(20, 6)
-    assert max(result) - min(result) <= 1
+    assert (max(split_integer(20, 6))
+            - min(split_integer(20, 6)) <= 1)
 
 
 def test_number_of_parts_should_be_exactly_as_requested() -> None:
